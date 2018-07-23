8723ae2a3e2c9f670d1939d046cad0dc99df4b44
Harsh Kotak <kotak_harsh@hotmail.com>
2018-07-23 11:53:39 -1000

    completed exercise

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,40 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
+function linkedListGenerator() {
+  let head = null;
+  let tail = null;
 
+  const getHead = () => {
+    return head;
+  };
+  const getTail = () => {
+    return tail;
+  };
+  const add = value => {
+    const newNode = value;
+    console.log("node: ", newNode);
+
+    if (head) {
+      head = newNode;
+      tail = newNode;
+      return head;
+    } else if (head !== null && tail === null) {
+      tail.next = newNode;
+      tail = newNode;
+      return tail;
+    }
+  };
+  const get = number => {};
+  const remove = number => {};
+  const insert = (number, value) => {};
+
+  return {
+    getHead,
+    getTail,
+    add,
+    get,
+    remove,
+    insert
+  };
 }
